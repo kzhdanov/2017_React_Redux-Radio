20fba2e7c78dd31fd797c1d5f81264ead4c50359
kj <pinchv@gmail.com>
2017-04-02 12:39:17 +0300

    somechange instructure

--- public/dev/js/components/header_component.js
+++ public/dev/js/components/header_component.js
@@ -1,57 +1,26 @@
 import React, { Component } from 'react';
-import Weeks from './weeks_component';
 
 class Header extends Component {
-	constructor(props) {
-	  super(props);
-	  
-	  this.state = {
-        classIcon: '',
-        classContainer: 'wk_container-invisible',
-        classVisible: '',
-  	  };
-	}
-
-	openCardWeek() {
-		if(this.state.classContainer === 'wk_container-invisible') {
-			this.setState({
-				classIcon: 'wk_icon-active',
-				classContainer: 'wk_container-visible',
-				classVisible: 'wk_openblock-invisible',
-			});
-		} else {
-			this.setState({
-				classIcon: '',
-				classContainer: 'wk_container-invisible',
-				classVisible: '',
-			});
-		}
-		/*
-		<div className={this.state.classIcon + ' wk_icon'}
-	      		 onClick={this.openCardWeek.bind(this)}>Недели</div>
-		*/
-	}
-
 	render() {
-		const isOpened = this.state.classContainer === 'wk_container-visible';
+		const isOpened = this.props.classContainer === 'wk_container-visible';
 	    return (
 	      <section className='header'>
-	      	<div onClick={this.openCardWeek.bind(this)} className='menu-container'>
-		      	<div id='menu-button' className={this.state.classVisible}>
+	      	<div onClick={this.props.openCardWeek} className='menu-container'>
+		      	<div id='menu-button' className={this.props.classVisible}>
 		      		<div className='menu-line' id='first-line'></div>
 		      		<div className='menu-line' id='second-line'></div>
 		      		<div className='menu-line' id='third-line'></div>
 		      	</div>
-		      	<div className={this.state.classIcon + ' close_popupX'}>
+		      	<div className={this.props.classIcon + ' close_popupX'}>
 		      		<div className='rotate_left'></div>
 		      		<div className='rotate_right'></div>
 		      	</div>
-		      	<div className={this.state.classIcon + ' menu-text'}>НЕДЕЛИ</div>
+		      	<div className={this.props.classIcon + ' menu-text'}>НЕДЕЛИ</div>
 		    </div>
 
-	      	<div className={this.state.classContainer + ' wk_container'}>
+	      	<div className={this.props.classContainer + ' wk_container'}>
 		      	{isOpened ? (
-			        <Weeks />
+			        this.props.renderWeeks()
 			    ) : (
 			        <span>no any information...</span>
 			    )}
